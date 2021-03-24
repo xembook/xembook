4e924a99209a3bdffcdd387e6552d80a7ba9d16a
xembook <mediaprogramer+xembok@gmail.com>
2021-03-24 18:43:34 +0900

    update

--- xembook.js
+++ xembook.js
@@ -1,16 +1,53 @@
 const NODES = [
-"https://symbol-harvest-node.com:3001",
+"https://0-0symbol-node1.trivill.com:3001",
+"https://00.symsym.info:3001",
+"https://01.symsym.info:3001",
+"https://02.symsym.info:3001",
+"https://03.symsym.info:3001",
+"https://04.symsym.info:3001",
+"https://sym-main.opening-line.jp:3001",
+"https://888.symsym.info:3001",
+"https://symbol.nagoya:3001",
+"https://age01.kitsutsuki.tokyo:3001",
+"https://age02.kitsutsuki.tokyo:3001",
+"https://02.symbol-node.net:3001",
+"https://00.symbol-node.net:3001",
+"https://symbol03.node.oe-jpy.com:3001",
+"https://07.symbol-node.net:3001",
 "https://node1.xym-harvesting.com:3001",
 "https://node2.xym-harvesting.com:3001",
-"https://symbol01.harvestasya.com:3001",
+"https://xym.idol-library.jp:3001",
+"https://08.symbol-node.net:3001",
 "https://00.harvester.earth:3001",
+"https://09.symbol-node.net:3001",
 "https://symbol-harvesting.com:3001",
+"https://05.symbol-node.net:3001",
+"https://03.symbol-node.net:3001",
+"https://symbol02.node.oe-jpy.com:3001",
+"https://06.symbol-node.net:3001",
+"https://04.symbol-node.net:3001",
 "https://symbol-node.net:3001",
+"https://ik1-449-56512.vs.sakura.ne.jp:3001",
+"https://sym-node-102.sakurairo.tokyo:3001",
 "https://shikinami.starlight.tokyo:3001",
+"https://01.symbol-node.net:3001",
+"https://ik1-421-42893.vs.sakura.ne.jp:3001",
 "https://xym.harvester.earth:3001",
+"https://ik1-432-48497.vs.sakura.ne.jp:3001",
+"https://symbol01.harvestasya.com:3001",
+"https://symbol-node.bakobox.net:3001",
+"https://cryptocat-xym-node.com:3001",
+"https://01.symbol.enoki-do.com:3001",
 "https://harvest-01.symbol.farm:3001",
+"https://harvest-02.symbol.farm:3001",
+"https://harvest-03.symbol.farm:3001",
+"https://symbol.harvest-monitor.com:3001",
+"https://nemauthn.harvestfield.tokyo:3001",
 "https://hideyoshi-node.net:3001",
+"https://symbol.from.nagoya:3001",
 "https://symbol-sakura-16.next-web-technology.com:3001",
+"https://ik1-438-51340.vs.sakura.ne.jp:3001",
+"https://super-harvester.com:3001",
 "https://sym-main-01.opening-line.jp:3001",
 "https://sym-main-02.opening-line.jp:3001",
 "https://sym-main-03.opening-line.jp:3001",
@@ -21,8 +58,11 @@ const NODES = [
 "https://sym-main-08.opening-line.jp:3001",
 "https://sym-main-09.opening-line.jp:3001",
 "https://sym-main-10.opening-line.jp:3001",
-"https://sym-main.opening-line.jp:3001",
+"https://sym-main-11.opening-line.jp:3001",
+"https://symbol-harvest-node.com:3001",
 "https://symbol-imog.tk:3001",
+"https://symbol-node-01.kokichi.tokyo:3001",
+"https://symbol01.master-ryzen00.trade:3001",
 "https://d3rmzi6ltfh1jy.cloudfront.net",
 "https://a.symbol.lcnem.net",
 ];
@@ -156,37 +196,10 @@ async function createRepo(d2){
 
 	accountInfo
 	.subscribe(_=>{
-
-		var accountImportance = Number(_.importance.toString()) / totalChainImportance ;
-		var provabilityHarvest = "";
-		if(accountImportance > 0){
-
-			accountImportance = Math.round( accountImportance );
-			accountImportance /= 1000000;
-			var activeImportance = accountImportance / ACTIVE_IMPORTANCE_MULTIPLIER;
-
-			p = 1 - activeImportance;
-			b = 1
-			while(0.5 < p){
-				p = p * (1 - activeImportance);
-				b += 1;
-			}
-			pHalf = b / 2880 ;
-
-			while(0.15 < p){
-				p = p * (1 - activeImportance);
-				b += 1;
-			}
-			pSig = b / 2880;
-			if(pHalf > 1){
-				provabilityHarvest = "[50%: " + pHalf.toFixed(1) + "日, 85%: " + pSig.toFixed(1) + "日]"
-			}
-		}
-
-		$("#account_importance").append("<dd>" + accountImportance + " " + provabilityHarvest + "</dd>");
 		getTransfers();
 		getHarvests();
 		getRecipets();
+
 		appendInfo(_);
 	});
 
